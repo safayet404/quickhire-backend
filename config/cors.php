282c50c8293e7d559d8a3ae84946fd5a673cacf9
safayet404 <hossainsafayet187@gmail.com>
2026-03-13 03:46:13 +0600

    Fix CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,14 @@
 <?php
 
+
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'https://quickhire-frontend-sable.vercel.app/')],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://quickhire-frontend-sable.vercel.app',
+        env('FRONTEND_URL', 'http://localhost:3000'),
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
