5001f83ef66b93975f9c76793d93ae343d9e9a58
safayet404 <hossainsafayet187@gmail.com>
2026-03-13 03:41:59 +0600

    cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'https://quickhire-frontend-sable.vercel.app/')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
